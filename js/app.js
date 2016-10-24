98fcdc9de80728c2dd88fb39ffd38a69cfa816f3
Arden Kim <kmk3170@uw.edu>
2016-10-24 11:10:59 -0700

    First half

--- js/app.js
+++ js/app.js
@@ -15,6 +15,9 @@ var songs = [{
     preview_url: "https://p.scdn.co/mp3-preview/8f1e48f29b418c274ce96fd6ae0f6b5002aefe02",
   }];
 
+var nowPlaying = new Audio();
+
+
 /**
  * This function takes in an object representing a SINGLE music track
  * and displays it on the page.
@@ -23,19 +26,19 @@ function renderTrack(track){
 
   //select the `.records` section that will contain the records
   //you could also do this before the function declaration
-
+  var records = document.querySelector(".records");
 
   //create a new `img` element to visually represent the track
-
+  var imgElement = document.createElement('img');
 
   //set the image's `src` to be the track's `album_image`
-
+  imgElement.setAttribute('src', track.album_image);
 
   //always give images `alt` attributes! The `name` of the track will work fine
-
+  imgElement.setAttribute('alt', track.name);
 
   //attach the image element to the `.records` section
-
+  records.appendChild(imgElement);
 
   //add an event listener so that when the track is clicked on,
   //it starts to spin! (add the Font-Awesome utility class `fa-spin`)
@@ -44,10 +47,24 @@ function renderTrack(track){
   //to change the element that was clicked on!
   //
   //Bonus: can you make the track stop spinning when clicked again?
+  imgElement.addEventListener('click', function(event) {
+    var whichRecord = event.target;
 
+    if (nowPalying.src === track.preview_url){
+      if(nowPlaying.paused) {
+        nowPlaying.play();
+      } else {
+        nowPlaying.pause();
+      }
+    } else {
+      nowPlaying = new Audio(track.preview_url);
+    }
+
+    whichRecord.classList.toggle('fa-spin');
+  });
 
 } //end method
 
 //call this function `forEach` element in the `songs` array!
-
+songs.forEach(renderTrack);
 
